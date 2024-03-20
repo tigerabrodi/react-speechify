1b3aeaea7bea14b5f78c69d428d24686b2ae97bd
Tiger Abrodi <tigerabrodi@gmail.com>
2024-03-20 11:44:07 +0100

    feat: introduce core components and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const synthRef = useRef(window.speechSynthesis)
+  const [voices, setVoices] = useState<SpeechSynthesisVoice[]>([])
+
+  useEffect(() => {
+    function setVoicesList() {
+      setVoices(synthRef.current.getVoices())
+    }
+
+    if (synthRef.current.onvoiceschanged !== undefined) {
+      synthRef.current.onvoiceschanged = setVoicesList
+    }
+
+    setVoicesList()
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1>Speechify</h1>
+      <textarea
+        name="speech"
+        aria-label="speech"
+        id="text-input"
+        placeholder="Type something here..."
+      />
+
+      <select name="voice" id="voice-select" aria-label="voice">
+        {voices.map((voice) => (
+          <option value={voice.name}>{voice.name}</option>
+        ))}
+      </select>
+
+      <button type="button" onClick={() => console.log('clicked')}>
+        SYNTHESIZE SPEECH
+      </button>
+    </main>
   )
 }
 
